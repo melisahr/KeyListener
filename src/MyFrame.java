6a535176cd9d58b4aaef52ba5a4e444a59416102
Melisa <melisahrnn@gmail.com>
2023-05-26 21:03:48 -0400

    Java File Finalize

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,4 @@
 package src;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -7,6 +6,7 @@ import java.awt.event.KeyListener;
 
 public class MyFrame extends JFrame implements KeyListener{
     JLabel label;
+    ImageIcon icon = new ImageIcon("rocket.png");
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,15 +14,15 @@ public class MyFrame extends JFrame implements KeyListener{
         this.setLayout(null);//allows to manually move components with keystrokes
         this.addKeyListener(this);
 
-        label =  new JLabel();
-        label.setBounds(0,0,100,100);
-        label.setBackground(Color.blue);
-        label.setOpaque(true);
+        icon = new ImageIcon("rocket.png");
 
+        label = new JLabel();
+        label.setBounds(0,0,100,100);
+        label.setIcon(icon);
+        this.getContentPane().setBackground(Color.black);
         this.add(label);
         this.setVisible(true);
     }
-
     @Override
     public void keyTyped(KeyEvent e) {
         //Invoked when a key is typed
@@ -33,15 +33,24 @@ public class MyFrame extends JFrame implements KeyListener{
                 break;
             case 's': label.setLocation(label.getX(), label.getY()+10);//move down
                 break;
-            case 'd': label.setLocation(label.getX()+10,label.getY());
+            case 'd': label.setLocation(label.getX()+10,label.getY());//move right
+                break;
         }
     }
-
     @Override
     public void keyPressed(KeyEvent e) {
         //Invoked when a physical key is pressed down
+        switch (e.getKeyCode()){
+            case 37: label.setLocation(label.getX()-10, label.getY());//move arrow left
+                break;
+            case 38: label.setLocation(label.getX(), label.getY()-10);//move arrow up
+                break;
+            case 39: label.setLocation(label.getX()+10, label.getY());//move arrow right
+                break;
+            case 40: label.setLocation(label.getX(),label.getY()+10);//move arrow down
+                break;
+        }
     }
-
     @Override
     public void keyReleased(KeyEvent e) {
         //called whenever key is removed
